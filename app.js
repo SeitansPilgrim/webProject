c14376b0e4a44df5d3246f133373295177549ea1
SeitansPilgrim <90258714+SeitansPilgrim@users.noreply.github.com>
2021-09-23 16:27:06 +0200

    arr mot DB

--- app.js
+++ app.js
@@ -1,14 +1,52 @@
 const dummyData = require('./dummydata')
+
 const express = require('express')
 const expressHandlebars = require('express-handlebars')
 const { response } = require('express')
 
 const app = express()
 
+//database.sqlite3 -------------------------------------------------------------------------
+const sqlite3 = require('sqlite3')
+const db = new sqlite3.Database('database.sqlite3')
+
+
+app.get('/recipes', function(request, response){
+	
+	db.all("SELECT * FROM Post", function(error, Post){
+		
+		if(error){
+			
+			const model = {
+				hasDatabaseError: true,
+				Post: []
+			}
+			response.render('recipes.hbs', model)
+			
+		}else{
+			
+			const model = {
+				hasDatabaseError: false,
+				Post
+			}
+			response.render('recipes.hbs', model)
+			
+		}
+	})
+})
+
+
+
+//----------------------------------------------------------------------------------------
+
+
+
+//Default layout
 app.engine("hbs", expressHandlebars({
     defaultLayout: 'main.hbs'
 }))
 
+// Links----------------------------------------------------------------------------------
 app.get('/', function(request, response){
     const model = {
         humans: dummyData.humans, 
@@ -23,10 +61,6 @@ app.get('/about', function(request,response){
 
 app.get('/contact', function(request,response){
     response.render('contact.hbs')
-})
-
-app.get('/recipes', function(request,response){
-    response.render('recipes.hbs')
 })
 
 app.listen(8080)
