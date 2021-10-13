8d4a1e1fcf432819d0336905ef4ac0daeec642a3
SeitansPilgrim <90258714+SeitansPilgrim@users.noreply.github.com>
2021-10-13 14:46:22 +0200

    hash
    hashing password

--- routers/authRouter.js
+++ routers/authRouter.js
@@ -1,10 +1,14 @@
+const { request } = require('express')
 const express = require('express')
+const bcrypt = require('bcryptjs')
 
 const router = express.Router()
 
 const ADMIN_USERNAME = 's'
 const ADMIN_PASSWORD = 's'
+const HASH_PASSWORD = bcrypt.hashSync(ADMIN_PASSWORD, 8)
 
+console.log(HASH_PASSWORD)
 
 router.get('/login', function (request, response) {
     response.render('login.hbs')
@@ -16,7 +20,9 @@ router.post('/login', function (request, response) {
     const username = request.body.username
     const password = request.body.password
 
-    if (username == ADMIN_USERNAME && password == ADMIN_PASSWORD) {
+    console.log(HASH_PASSWORD)
+
+    if (username == ADMIN_USERNAME && bcrypt.compare(password, HASH_PASSWORD)) {
         request.session.isLoggedIn = true
         // TODO: Do something better than redirecting to start page.
         response.redirect('/')
@@ -26,7 +32,6 @@ router.post('/login', function (request, response) {
         // TODO: Display error message to the user.
         response.render('login.hbs')
     }
-
 })
 
 module.exports = router
